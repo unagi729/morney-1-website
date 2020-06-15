3b13b2cd2e549b26ce8f06a06939a3f7c21ea37f
mandy <535286768@qq.com>
2020-06-16 00:54:50 +0800

    deploy

---
+++ precache-manifest.d6e423ec3e1d983c8d43cbd5301d46f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7e1fee5bb55f4f0874",
+    "url": "/morney-1-website/css/app.73e6c6dd.css"
+  },
+  {
+    "revision": "fa42550a62bd1b20b45d2819f76280ec",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "ac7e1fee5bb55f4f0874",
+    "url": "/morney-1-website/js/app.1d2d3b38.js"
+  },
+  {
+    "revision": "955e31d9bca1140a05b1",
+    "url": "/morney-1-website/js/chunk-vendors.86b4cb5f.js"
+  },
+  {
+    "revision": "3464f02e99d185670393e1afc9626e1f",
+    "url": "/morney-1-website/lib/clone.ts"
+  },
+  {
+    "revision": "004098265d46e2e03499ea5912466aaa",
+    "url": "/morney-1-website/lib/createId.ts"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "62d57711dbf5273e13bb4e6cf48976d6",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
